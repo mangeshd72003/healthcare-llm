8634f9476870841584d7b2420c13125804e4e647
mangeshd72003 <156631452+mangeshd72003@users.noreply.github.com>
2025-01-25 12:33:23 +0530

    Add files via upload

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-#from langchain_huggingface import HuggingFaceEmbeddings
-#from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
@@ -30,13 +30,13 @@ print("Length of Text Chunks: ", len(text_chunks))
 
 # Step 3: Create Vector Embeddings 
 
-#def get_embedding_model():
-    #embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    #return embedding_model
+def get_embedding_model():
+    embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embedding_model
 
-#embedding_model=get_embedding_model()
+embedding_model=get_embedding_model()
 
 # Step 4: Store embeddings in FAISS
-#DB_FAISS_PATH="vectorstore/db_faiss"
-#db=FAISS.from_documents(text_chunks, embedding_model)
-#db.save_local(DB_FAISS_PATH)
+DB_FAISS_PATH="vectorstore/db_faiss"
+db=FAISS.from_documents(text_chunks, embedding_model)
+db.save_local(DB_FAISS_PATH)
